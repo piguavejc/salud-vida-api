c38380b84bcc23339b69b6de7652034f90826a1f
Jean carlos <piguavejc@gmail.com>
2025-09-07 20:28:39 +0000

    Add deepmerge dependency and implement createCustomResolver utility function

---
+++ src/shared/lib/util.ts
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+// import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import * as deepmerge from 'deepmerge';
+
+import {
+  AutoResolverOpts,
+  PagingStrategies,
+  ReadResolverOpts,
+} from '@nestjs-query/query-graphql';
+
+import { JwtAuthGuard } from 'src/shared/modules/jwt/guard/jwt.guard';
+
+type CRUDResolverOptions = AutoResolverOpts<
+  unknown,
+  unknown,
+  unknown,
+  unknown,
+  ReadResolverOpts<unknown>,
+  PagingStrategies
+>;
+
+type InitialResolverOptions = Omit<
+  CRUDResolverOptions,
+  'ServiceClass' | 'DTOClass'
+>;
+
+const initialResolver: InitialResolverOptions = {
+  enableTotalCount: true,
+  pagingStrategy: PagingStrategies.OFFSET,
+  guards: [JwtAuthGuard],
+  create: { many: { disabled: true } },
+  update: { many: { disabled: true } },
+  delete: { many: { disabled: true } },
+};
+
+export const createCustomResolver = (
+  options: CRUDResolverOptions,
+): CRUDResolverOptions => {
+  // @ts-ignore
+  return deepmerge(initialResolver, options);
+};
