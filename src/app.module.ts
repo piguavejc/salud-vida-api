d71f6194879e5ea9a1b8cfcccab1ab74e32e76bf
Jean carlos <piguavejc@gmail.com>
2025-09-07 20:34:14 +0000

    Add UserModule and ImageModule imports to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { ConfigModule } from '@nestjs/config';
 import { GraphqlConfigModule } from 'src/shared/modules/graphql/graphql.module';
+import { ImageModule } from 'src/modules/image/image.module';
 import { JwtModule } from 'src/shared/modules/jwt/jwt.module';
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import { Module } from '@nestjs/common';
 import { RbacModule } from 'src/shared/modules/rbac/rbac.module';
 import { TypeOrmConfigModule } from 'src/shared/modules/typeorm/typeorm.module';
+import { UserModule } from 'src/modules/user/user.module';
 import { envSchema } from 'src/config/config';
 
 @Module({
@@ -22,6 +24,8 @@ import { envSchema } from 'src/config/config';
     GraphqlConfigModule,
     JwtModule,
     RbacModule,
+    UserModule,
+    ImageModule,
   ],
   controllers: [AppController],
   providers: [AppService],
